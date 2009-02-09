8633068cb18a449d1ab3cab6e6d114c52dc50859
patrick.osullivanphd <patrick.osullivanphd@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-02-09 22:33:34 +0000

    test from pc WORKING

--- trunk/testasd/src/helloworld.java
+++ trunk/testasd/src/helloworld.java
@@ -7,6 +7,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello");
 	}
 
 }
