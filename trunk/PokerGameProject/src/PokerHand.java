c195b00a36ecb458c214a39d7992759ccffd68a6
patrick.osullivanphd <patrick.osullivanphd@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-02-10 16:36:17 +0000

    IsOnepair added and working

--- trunk/PokerGameProject/src/PokerHand.java
+++ trunk/PokerGameProject/src/PokerHand.java
@@ -1,8 +1,7 @@
 
 public class PokerHand {
 
-	
-	
+
 	public static void bubbleSort(Card[] x) {
 	    int n = x.length;
 	    for (int i=1; i < n; i++) {  // count how many times
@@ -20,11 +19,35 @@ public class PokerHand {
 	}
 
 	public static boolean IsOnePair(Card[] x){
-		for(int i; i < x.length; i++){
-			if()
+		boolean IsOnePair = false;
+		Card match;
+		for(int i = 0; i < x.length -1; i++){
+			if( (x[i].getValue()) == (x[i+1].getValue())){
+				
+				System.out.println(" One pair found. " + x[i] +" and " +x[i+1]);
+				IsOnePair = true;
+				match = x[i+1];
+				//return true;
+			}
+			else{
+				//return false;
+			}
 		}
+		if(IsOnePair == false){
+		System.out.println(" No pair found.");
+		}
+		//return false;	
+		return IsOnePair;
 	} 
 	
+	
+	
+	public static void IsTwoPair(Card[] x){
+		if(IsOnePair(x) == true && IsOnePair(x) == true){
+			IsOnePair.match
+			System.out.println(" two pair found. " );
+		}
+	}
 	
 	public static void main(String args[]){
 	Deck deck = new Deck();
@@ -38,6 +61,10 @@ public class PokerHand {
 			System.out.println(cardHand[i]);
 		}
 	
+		IsTwoPair(cardHand);
+		//if (IsOnePair(cardHand) == true){
+		//	System.out.println(" One pair found.");
+		//}
 	}
 	
 	
