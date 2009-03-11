015ecb011620521e42ba01fccfa3e82a17c0ebcd
patrick.osullivanphd <patrick.osullivanphd@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-03-11 18:09:14 +0000

    added basic structure to pokergame project

--- PokerGameProject/HumanPlayer.java
+++ PokerGameProject/HumanPlayer.java
@@ -1,30 +1,3 @@
 
-public class HumanPlayer extends PokerHand {
-	int decision;
-	int bet=0;
-	int currentMoney;
-	Card[] thisHand;
-
-		public AI_plyr(String Name, int Chips, Card[] currentHand){
-			
-		}
-		
-		public int getDecision(){ // public methods for OOP to get our decision and amount to bet (if any)
-			return  decision // 0=fold,1=check/call, 2=raise
-		}
-		public int getBet(){
-			if(decision!=0){
-				return bet;
-			}
-			
-		}
-		
-		
-		private makeDecision(thisHand){ // where we decide what to do - keep private!
-			
-		}
-		
-		private int makeBet(){ // where we decide what to bet(if any) - keep private!
-			return bet;
-		}
-}
+public class HumanPlayer implents Player {
+	}
