57a634981d58130ab308c1601f3e39c78bb54a6a
neilmccarthy1 <neilmccarthy1@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-03-13 04:26:41 +0000

    A few additions to the Player class.

--- PokerGameProject/Player.java
+++ PokerGameProject/Player.java
@@ -9,7 +9,7 @@ public class Player extends PokerHand {
 
 	
 	public player(String Name, int Chips, Card[] currentHand){
-		
+		Chips = currentMoney;
 	}
 	
 	// this takes ah array of dealt cards then adds them to the
@@ -32,7 +32,30 @@ public class Player extends PokerHand {
 		}
 	}
 	
-	public int getDecision(){ // public methods for OOP to get our decision and amount to bet (if any)
+	
+	public void fold(){
+		//When a player folds, they lose the current hand, and their money in the
+		//pot goes to the winner of the hand.
+	}
+	
+	public void raise(float amount_raised){
+		//A player can raise by any amount, on their turn pre-flop, after the flop,
+		//after the turn, or after the river, whereby the the other players must call,
+		//raise or fold.
+	}
+	
+	public void check(){
+		//A player can check on their turn without betting if no bet has been placed
+		//yet.
+	}
+	
+	public void call(float amount_checked){
+		//A player calls by matching the amount raised by the previous player.
+	}
+	
+	
+// Don't think these methods are necessary?!
+/*	public int getDecision(){ // public methods for OOP to get our decision and amount to bet (if any)
 		return  decision // 0=fold,1=check/call, 2=raise
 	}
 	
@@ -49,5 +72,5 @@ public class Player extends PokerHand {
 	
 	private int makeBet(){ // where we decide what to bet(if any) - keep private!
 		return bet;
-	}
+	}*/
 }
