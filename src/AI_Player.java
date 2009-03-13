0a622969bbcd6cbe6c469a84625528732c6d096f
patrick.osullivanphd <patrick.osullivanphd@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-03-13 17:09:13 +0000

    added to player

--- src/AI_Player.java
+++ src/AI_Player.java
@@ -4,5 +4,15 @@ public class AI_Player extends Player {
 	public AI_Player(String Name, int Chips, Card[] currentHand) {
 		super(Name, Chips, currentHand);
 		// TODO Auto-generated constructor stub
+		
 	}
+	
+	
+	public int getBet(){
+		
+		return 20;
+		
+	}
+	
+	
 	}
--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,11 @@ public class Player extends Pack {
 	int bet=0;
 	int currentMoney;
 	Card[] thisHand = new Card[7];
+	Card[] playerHand = new Card[2];
+	Card[] communityCards = new Card[5];
 	int cardCount=0;
+	int cardPocketCount=0;
+	
 
 	
 	public Player(String Name, int Chips, Card[] currentHand){
@@ -16,21 +20,51 @@ public class Player extends Pack {
 	// players hand. using cardCount to make sure its in the 
 	// position
 	public void receiveCards(Card[] received){
+		
+		if( cardCount >= 3){
+			Card[] tempCopy = new Card[(communityCards.length)];
+			System.arraycopy(communityCards, 0, tempCopy, 0, tempCopy.length);
+			communityCards = new Card[(communityCards.length+1)];
+			System.arraycopy(tempCopy, 0,communityCards , 0, communityCards.length);
+		}
+		
 		Card[] temp = received;
 		for(int i = 0;i <temp.length;i++){
-			thisHand[cardCount] = temp[i];
+			communityCards[cardCount] = temp[i];
 			cardCount++;
+		}
+		
+		
+	}
+	
+	// this takes ah array of dealt cards then adds them to the
+	// players hand.
+	// This is just for the first two pocket cards a player gets
+	public void receivePocketCards(Card[] received){
+		Card[] temp = received;
+		for(int i = 0;i <temp.length;i++){
+			playerHand[cardCount] = temp[i];
+			cardPocketCount++;
 		}
 	}
 	
 	
-	public void printHand(){
+	public void printPocketHand(){
 	
 		for(int i = 0; i < 2; i++){
-			System.out.println(thisHand[i].getValueAsString()+ " of " + thisHand[i].getSuitAsString());
+			System.out.println(playerHand[i].getValueAsString()+ " of " + playerHand[i].getSuitAsString());
 
 		}
 	}
+	
+	public void printHand(){
+		
+		for(int i = 0; i < 5; i++){
+			System.out.println("community Cards are :" +communityCards[i].getValueAsString()+ " of " + communityCards[i].getSuitAsString());
+
+		}
+	}
+	
 	
 	public int getDecision(){ // public methods for OOP to get our decision and amount to bet (if any)
 		return  decision; // 0=fold,1=check/call, 2=raise
