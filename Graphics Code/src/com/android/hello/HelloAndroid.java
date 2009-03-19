3eb2bbbcb9ec346b9fb5a2789589eb206e09bb53
ciaran101 <ciaran101@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-03-19 17:45:11 +0000

    hello

---
+++ Graphics Code/src/com/android/hello/HelloAndroid.java
@@ -0,0 +1,17 @@
+package com.android.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloAndroid extends Activity {
+   /** Called when the activity is first created. */
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       TextView tv = new TextView(this);
+       tv.setText("Hello, Aggd");
+       System.out.println("abc");
+       setContentView(tv);
+   }
+}
