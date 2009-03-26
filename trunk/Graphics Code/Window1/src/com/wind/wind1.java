94614915a16fbdb3b6f9abb76b898dd2cfffe4ca
ciaran101 <ciaran101@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-03-26 22:32:47 +0000

    Starts up in landscape mode. (ctrl+f12 to rotate emulator) And added working buttons.

--- trunk/Graphics Code/Window1/src/com/wind/wind1.java
+++ trunk/Graphics Code/Window1/src/com/wind/wind1.java
@@ -4,62 +4,63 @@ import com.wind.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.os.Message;
-import android.util.AttributeSet;
-import android.view.KeyEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ImageView.ScaleType;
 
-
-//import android.content.pm.ActivityInfo;
-//import android.os.ServiceManager;
-
-public class wind1 extends Activity {
+public class wind1 extends Activity{
     
-    Button deal;
-    Button bet;
-    Button fold;
+    private Button deal;
+    private Button bet;
+    private Button fold;
+    private TextView bestHand;
+    private TextView pot;
     
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
     	
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); 
-
         setContentView(R.layout.main);
-        
-        Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), R.drawable.table); 
-        //int width = bitmapOrg.getWidth();
-        //int height = bitmapOrg.getHeight();
-        BitmapDrawable bmd = new BitmapDrawable(bitmapOrg);
         ImageView imageView = (ImageView)findViewById(R.id.img1);; 
         imageView.setScaleType(ScaleType.FIT_START);
-        
         deal = (Button)this.findViewById(R.id.but1);
+		deal.setOnClickListener(l_deal);
         bet = (Button)this.findViewById(R.id.but2);
+		bet.setOnClickListener(l_bet);
         fold = (Button)this.findViewById(R.id.but3);
-        
-
+		fold.setOnClickListener(l_fold);
+        bestHand = (TextView)this.findViewById(R.id.textv3);
+        pot = (TextView)this.findViewById(R.id.textv2);
     }
-
+    private OnClickListener l_deal = new OnClickListener() {
+        public void onClick(View v) {
+        	// code to do something when the deal button is pressed.
+        	pot.setText("€1");
+        }
+    };
+    private OnClickListener l_bet = new OnClickListener() {
+        public void onClick(View v) {
+        	//...
+        	pot.setText("€2.50");
+        }
+    };
+    private OnClickListener l_fold = new OnClickListener() {
+        public void onClick(View v) {
+        	//...
+        	pot.setText("€43.75");
+        }
+    };
+    public void setBestHandText(String s){
+        bestHand.setText(s);
+    }
+    public void setPotText(String s){
+        pot.setText(s);
+    }
 }
