b6ccc0c3731db12406d6251aba22ceefb46f7cf5
ciaran101 <ciaran101@6479980e-ecaa-11dd-9cf0-a157cfa833bd>
2009-04-16 13:02:15 +0000

    Main window

---
+++ trunk/Graphics Code/New Folder/src/com/wind/PokerHand.java
@@ -0,0 +1,463 @@
+package com.wind;
+
+
+public class PokerHand extends Pack { // Not really working for this weeks assignment - Doesn't pick up on improved hands for anything more than 2 pair
+
+
+
+
+	Card[] theseCards;
+	Card[] mySortedCards;
+	
+	int[] matchedCards;
+	double Pair=0;
+	double twoPair=0;
+	double Threes=0;
+	double Fours=0;
+	double fullHouse=0;
+	double Straight=0;
+	double Flush=0;
+	double SF=0;
+	double SRoyalFlush=0;
+	int testAmount = 1;
+	int threeCard;
+	int fourCard;
+	int matchCardCount=0;
+	int firstMatch;
+	int secondMatch;
+	int flushCount=0;
+	int flushSuit;
+	boolean almostFlush=false;
+	boolean printedBoolean=false;
+	int[][] cardStats = new int[10][3];
+	int highestSecondPair;
+	
+
+
+
+		public PokerHand(Card[] Hand) {
+		// TODO Auto-generated constructor stub
+			Card[] theseCards = Hand; 	
+			mySortedCards = sortedCards(theseCards);
+			//System.out.println("PokerHand created is:");
+			//for(int i=0;i<Hand.length;i++){
+				
+				//System.out.println(Hand[i].getValueAsString()+" "+Hand[i].getSuitAsString());
+			//}
+			
+		}
+		
+	public int testBooleans(){
+		//tests for different winning hands.
+		if(isRoyalFlush()){
+			if(printedBoolean==false){
+				//System.out.println("Royal Flush!!!!");
+				SRoyalFlush++;
+				printedBoolean=true;
+			}
+
+
+			return 0;
+		}
+
+		if(isStraightFlush()){
+			if(printedBoolean==false){
+				//System.out.println("Straight flush");
+				printedBoolean=true;
+				SF++;
+			}
+
+
+			return 1;
+		}
+
+		if(isFourOfAKind()){
+			if(printedBoolean==false){
+				//System.out.println("Found four of a kind");
+				Fours++;
+				printedBoolean=true;
+			}
+
+			return 2;
+		}
+
+		if(isFullHouse()){
+			if(printedBoolean==false){
+				//System.out.println("Found a full house");
+				fullHouse++;	
+				printedBoolean=true;
+			}
+
+			return 3;
+		}
+		if(isFlush()){
+			if(printedBoolean==false){
+				//System.out.println("Found a flush");
+				Flush++;
+				printedBoolean=true;
+			}
+
+			return 4;
+		}
+		if(isStraight()){
+			if(printedBoolean==false){
+				//System.out.println("Found a straight");
+				Straight++;	
+				printedBoolean=true;
+			}
+
+			return 5;
+		}
+		if(isThreeOfAKind()){
+			if(printedBoolean==false){
+				//System.out.println("Found three of a kind");
+				Threes++;
+				printedBoolean=true;
+			}
+
+			return 6;
+		}
+		if(isTwoPair()){
+			if(printedBoolean==false){
+				//System.out.println("Found two pair");
+				twoPair++;
+				printedBoolean=true;
+			}
+
+			return 7;
+		}
+
+		if(isOnePair()){
+			if(printedBoolean==false){
+				//System.out.println("Found a pair");
+				Pair++;
+				printedBoolean=true;
+			}
+
+			return 8;
+		}
+
+		if(!isStraight()&&!isFullHouse()&&!isFourOfAKind()&&!isThreeOfAKind()
+				&&!isTwoPair()&&!isOnePair()&&!isFlush()){
+			if(printedBoolean==false){
+				//System.out.println("Found nothing");
+				printedBoolean=true;
+			}
+
+			return 9;
+		}
+		else{
+			return 9;
+		}
+
+
+	}
+
+
+
+	public Card[]  sortedCards(Card[] hand){ // sorts cards 
+		boolean doMore = true;
+		int n = hand.length;
+		while (doMore) {
+			n--;
+			doMore = false; 
+
+
+			for (int i=0; i<n; i++) {
+				if(((hand[i]).getValue())>((hand[i+1]).getValue())){
+					/*System.out.println(hand[i].getValue()+" "+hand[i].getSuitAsString());
+					System.out.println(hand[i+1].getValue()+" "+hand[i+1].getSuitAsString());
+					System.out.println(" ");*/
+					Card big = hand[i];
+					hand[i] = hand[i+1];
+					hand[i+1] = big;
+					doMore=true;
+					/*System.out.println(hand[i].getValue()+" "+hand[i].getSuitAsString());
+					System.out.println(hand[i+1].getValue()+" "+hand[i+1].getSuitAsString());
+					System.out.println(" ");*/
+				}
+				
+			}
+			/*for(int j=0;j<hand.length;j++){
+				System.out.println(hand[j].getValueAsString());*/
+				
+			/*}
+			System.out.println(" ");*/
+		}
+		/*System.out.println("Sorted:");
+		for(int i=0;i<hand.length;i++){
+			System.out.println(hand[i].getValueAsString()+ " of " + hand[i].getSuitAsString());
+		}*/
+
+		return hand;
+	}
+
+	public boolean isOnePair(){
+
+		for(int i=0;i<mySortedCards.length-1;i++){
+
+			if((mySortedCards[i]).getValue()==(mySortedCards[i+1].getValue())){
+				firstMatch = mySortedCards[i].getValue();
+				return true;
+			}
+
+		}
+
+		return false;
+
+	}
+	public boolean isTwoPair(){
+		isOnePair();
+	
+	
+	  for(int i=0;i<mySortedCards.length-1;i++){
+
+			if((mySortedCards[i]).getValue()==(mySortedCards[i+1].getValue())){
+				if((mySortedCards[i]).getValue()!=firstMatch&&mySortedCards[i+1].getValue()!=firstMatch){
+					secondMatch=mySortedCards[i].getValue();
+					
+					highestSecondPair=secondMatch;
+					return true;
+				}
+
+			}
+		}
+		return false;
+
+	}
+	public boolean isThreeOfAKind(){
+
+		for(int i=0;i<3;i++){
+
+			if((mySortedCards[i]).getValue()==(mySortedCards[i+1].getValue())&&(mySortedCards[i+1]).getValue()==(mySortedCards[i+2].getValue())){
+				threeCard = mySortedCards[i].getValue();
+				return true;
+
+			}
+
+		}
+
+		return false;
+
+	}
+	public boolean isFourOfAKind(){
+		for(int i=0;i<2;i++){
+
+			if((mySortedCards[i]).getValue()==(mySortedCards[i+1].getValue())&&(mySortedCards[i+1]).getValue()==(mySortedCards[i+2].getValue())&&(mySortedCards[i+2]).getValue()==(mySortedCards[i+3].getValue())){
+				fourCard = mySortedCards[i].getValue();
+				return true;
+
+			}
+
+		}
+
+		return false;
+
+	}
+	public boolean isFullHouse(){
+		for(int i=0;i<mySortedCards.length-1;i++){
+			if(mySortedCards[i].getValue()==mySortedCards[i+1].getValue()&&mySortedCards[i].getValue()!=threeCard&&isThreeOfAKind()){
+				return true;
+			}
+		}
+
+		return false;
+	}
+	public boolean isStraight(){
+		boolean smallStraight=false;
+		//Card[] theseSortedCards = sortedCards(theseCards);
+
+
+		if(mySortedCards[0].getValue()==0&&mySortedCards[1].getValue()==9&&mySortedCards[2].getValue()==10
+				&& mySortedCards[3].getValue()==11 && mySortedCards[4].getValue()==12){
+			smallStraight=true;
+			return smallStraight;
+		}
+		for(int i=0;i<mySortedCards.length-1;i++){
+			if(((mySortedCards[i+1]).getValue())-((mySortedCards[i]).getValue())==1){
+				smallStraight=true; // holds true for every iteration of for loop if value is always greater than the next
+			}
+
+			else{
+				smallStraight=false;
+				break; // no point in keeping checking if it breaks down once
+			}
+		}
+
+		return smallStraight;
+
+
+	}
+	public boolean isFlush(){
+
+
+		boolean noFlush=false;
+		for(int i=0;i<mySortedCards.length-1;i++){
+			if(((mySortedCards[i]).getSuit()) == ((mySortedCards[i+1]).getSuit())){
+				flushCount++;
+				//System.out.println("Current Flush count: "+flushCount);
+				flushSuit = mySortedCards[i].getSuit();
+				//smallFlush=true; // holds true for every iteration of for loop if suit is always the same as the next
+			}
+			else{
+				noFlush=true;
+				//break;
+			}
+		}
+		if(flushCount>=2){
+			almostFlush=true;
+		}
+		if(noFlush==true){
+			flushCount=0;
+			return false;
+		}
+		flushCount=0;
+		return true;
+
+	}
+
+	public boolean isStraightFlush(){
+		if(isFlush()&&isStraight()){
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isRoyalFlush(){
+		if(mySortedCards[0].getValue()==0&&mySortedCards[1].getValue()==9&&mySortedCards[2].getValue()==10
+				&& mySortedCards[3].getValue()==11 && mySortedCards[4].getValue()==12&&isFlush()){
+			return true;
+		}
+		return false;
+	}
+
+	public int shouldDiscard(int cardPosition) {
+		int discard;
+		if (testBooleans()==0){ // royal flush
+			return discard = 0;
+		}
+		if (testBooleans()==1){ //both very good hands - no need to change - straight flush
+			return discard = 0;
+		}
+		if (testBooleans()==2){//four of a kind
+			if (mySortedCards[cardPosition].getValue()!=fourCard){ //four of a kind
+				return discard=50;
+			}
+			else{
+				return discard=0;
+			}
+		}
+		if (testBooleans()==3){// full house
+			if (mySortedCards[cardPosition].getValue()!=threeCard){ //three of a kind
+				return discard=5;
+			}
+			else{
+				return discard=0;
+			}
+		}
+		if (testBooleans()==4){//flush - don't want to change
+			return discard=0;
+		}
+		if (testBooleans()==5){//straight - don't want to change
+			return discard=0;
+		}
+		if (testBooleans()==6){//three of a kind -
+			if (mySortedCards[cardPosition].getValue()!=threeCard){
+				return discard=50;
+			}
+			else{
+				return discard=0;
+			}
+		}
+		if (testBooleans()==7){//two pair - want to discard single card
+
+			if(mySortedCards[cardPosition].getValue()!=secondMatch&&mySortedCards[cardPosition].getValue()!=firstMatch){
+				return discard=75;
+
+			}
+			return discard=0;
+		}
+		if (testBooleans()==8){//pair - want to discard other cards
+
+			if(mySortedCards[cardPosition].getValue()!=firstMatch){
+				return discard=75;
+
+			}
+			return discard=0;
+		}
+
+		if(almostFlush==true&&mySortedCards[cardPosition].getSuit()!=flushSuit&&testBooleans()==9){ // almost flush - doesn't quite work
+			return discard=85;
+		}
+
+		if(mySortedCards[cardPosition].getValue()==0&&testBooleans()==9){ // got nothing going on except an ace so we'll keep that
+			return discard=35;
+		}
+		else{
+			return discard=80;
+		}
+
+
+	}
+
+	public Card getHighCard(){
+		if (mySortedCards[0].getValue()==0){ // check for an ace which counts as a 1 numerically
+			return mySortedCards[0];
+		}
+		else{
+			return mySortedCards[4];
+		}
+	}
+	
+	public Card getLowestHighCard()  {
+		
+		if(mySortedCards[0].getValue() == 0){
+			for(int i = 0; i < 5 ; i++){
+				if(mySortedCards[i].getValue() != 0){
+					return mySortedCards[i];
+				}
+			}
+		}
+		else {
+			return mySortedCards[0];
+		}
+		System.out.println("I'm throwing an error in the getLowestHighCard method!!");
+		return null;
+	}
+		
+		
+	
+	public Card [] getCard(){
+		return mySortedCards;
+	}
+	
+	public void printPokerHand(){
+		for (int i=0;i<mySortedCards.length;i++){
+			System.out.println(mySortedCards[i].getValueAsString()+" "+mySortedCards[i].getSuitAsString());
+		}
+	}
+	public Card[] firstTwo(){
+		Card [] myCard = new Card[2];
+		myCard [0] =mySortedCards[0];
+		myCard [1] =mySortedCards[1];
+		return myCard;
+		
+	}
+	
+	public int getLastCardValue(){
+		return mySortedCards[4].getValue();
+	}
+	
+	public int getNonPair(){
+		if(isTwoPair()){
+			for (int i=0;i<mySortedCards.length;i++){
+				if(mySortedCards[i].getValue()!= firstMatch&&mySortedCards[i].getValue()!= highestSecondPair){
+					return mySortedCards[i].getValue();
+				}
+			}
+		}
+		
+		return 0;
+	}
+}
---
+++ trunk/Graphics Code/New Folder/src/com/wind/pokerGameMethods.java
@@ -0,0 +1,75 @@
+package com.wind;
+import java.util.Random;
+
+
+public class pokerGameMethods extends Pack {
+
+	int bestBoolean=10;
+	
+public int chooseFirstDealer(int amtPlayers){
+		
+		Random rand = new Random();
+		int min=0;
+		int max=amtPlayers;
+
+		
+		int randomNum = rand.nextInt(max - min ) + min;
+
+		
+		return randomNum;
+		
+	}
+
+public int chooseNextDealer(int amtPlayers, int currentDealer){
+	
+	if(currentDealer==amtPlayers-1){ //if current dealer is last person in player array, go back to the first person
+		return 0;
+	}
+	else{
+		return currentDealer+1; // otherwise dealer is previous dealer +1
+	}
+}
+
+
+public Player winner(Player [] involvedPlayers, PokerHand [] possibleWinners){ // this takes in an array of pokerHands (which are the best 5 cards of a player) and determines the winning player
+	
+	PokerHand[] localpossibleWinners = possibleWinners;
+	
+	System.out.println("Cards passed for player 1 in are: ");
+	for(int i=0;i<localpossibleWinners[0].mySortedCards.length;i++){
+		System.out.println(localpossibleWinners[0].mySortedCards[i].getValueAsString()+" "+localpossibleWinners[0].mySortedCards[i].getSuitAsString());
+	}
+	
+	System.out.println("Cards passed for player 2 in are: ");
+	for(int i=0;i<localpossibleWinners[1].mySortedCards.length;i++){
+		System.out.println(localpossibleWinners[1].mySortedCards[i].getValueAsString()+" "+localpossibleWinners[1].mySortedCards[i].getSuitAsString());
+	}
+	
+	
+	
+	for(int i=0;i<localpossibleWinners.length;i++){
+		if(localpossibleWinners[i].testBooleans()<bestBoolean){ //go through pokerCard array and find best hand
+			bestBoolean=localpossibleWinners[i].testBooleans();
+			
+		}
+	}
+	
+	//PokerHand[] finalists = new PokerHand[involvedPlayers.length];
+	int finalistsHighCardCount=0;
+	int winningPlayer=0;
+	
+	for(int i=0;i<localpossibleWinners.length;i++){ // now need to check for any other players with the same hand and use the highest card to determine the winner
+		if(localpossibleWinners[i].testBooleans()==bestBoolean && (localpossibleWinners[i].getHighCard().getValue()>finalistsHighCardCount||localpossibleWinners[i].getHighCard().getValue()==0)){
+			finalistsHighCardCount = localpossibleWinners[i].getHighCard().getValue();
+			winningPlayer=i;
+		}
+	}
+	
+	return involvedPlayers[winningPlayer];
+	
+}
+
+
+	
+}
+
